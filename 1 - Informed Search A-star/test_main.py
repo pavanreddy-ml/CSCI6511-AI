b68cf3018d99f725748fde363ba8c8c809052103
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2023-02-24 00:39:06 -0500

    New Test

--- 1 - Informed Search A-star/test_main.py
+++ 1 - Informed Search A-star/test_main.py
@@ -4,9 +4,29 @@ from unittest import TestCase
 class Test(TestCase):
     def test_main(self):
         from main import main
-        print(main("Test Files/input.txt")[0])
-        print(main("Test Files/input1.txt")[0])
-        print(main("Test Files/input2.txt")[0])
-        print(main("Test Files/input3.txt")[0])
-        print(main("Test Files/input4.txt")[0])
+        # print(main("Test Files/input.txt")[0])
+        # print(main("Test Files/input1.txt")[0])
+        # print(main("Test Files/input2.txt")[0])
+        # print(main("Test Files/input3.txt")[0])
+        # print(main("Test Files/input4.txt")[0])
+        # print(main("Test Files/input5.txt")[0])
+        # print(main("Test Files/input6.txt")[0])
+
+        # print(main("Test Files/input.txt"))
+        # print(main("Test Files/input1.txt"))
+        # print(main("Test Files/input2.txt"))
+        # print(main("Test Files/input3.txt"))
+        # print(main("Test Files/input4.txt"))
+        # print(main("Test Files/input5.txt"))
+        # print(main("Test Files/input6.txt"))
+
+        main("Test Files/input.txt")
+        main("Test Files/input1.txt")
+        main("Test Files/input2.txt")
+        main("Test Files/input3.txt")
+        main("Test Files/input4.txt")
+        main("Test Files/input5.txt")
+        main("Test Files/input6.txt")
+
+
 
